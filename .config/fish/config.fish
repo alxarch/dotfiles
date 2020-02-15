7738d50ee6ca9e55b754f34d0cb005f357b00619
Sigalas Alexandros <alxarch@gmail.com>
2020-02-15 14:24:36 +0200

    [fish] Add abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,9 @@
 if functions -q fzf_key_bindings
 	fzf_key_bindings
 end
+set fish_greeting
+
+if status --is-interactive
+	abbr --add --global dotfiles 'env GIT_DIR="$HOME/.dotfiles" GIT_WORK_TREE="$HOME" git'
+	abbr --add --global rm 'rm -i'
+end
